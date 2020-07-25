7be3c352835e1a845256aa23824fd5eced646653
Mateus Pereira <mateuusth14@gmail.com>
2020-07-25 16:38:17 -0300

    Headers

--- Headers/aluno.h
+++ Headers/aluno.h
@@ -1,25 +1,25 @@
 #ifndef ALUNO_H_
 #define ALUNO_H_
-#endif
 
-//Define Regex
-#define TAM 100
-#define EXPR_REG "[A-z]{3,20}\\s[A-z]{2,20}"
-#define EXPR_REG2 "[A-z]{5,20}\\s[A-z]{2,20}|[A-z]{2,20}"
+    #define TAM 100 
+    #define EXPR_REG "[A-z]{3,20}\\s[A-z]{2,20}"
+    #define EXPR_REG2 "[A-z]{5,20}\\s[A-z]{2,20}|[A-z]{2,20}"
 
-typedef struct
-{
+    typedef struct
+    {
     int codigo;
     char nome[TAM], disciplina[TAM];
     float notas[3];
-} Aluno;
+    } Aluno;
 
-void acrescentarDados(Aluno aluno);
-void listarDados(Aluno aluno);
-void consultarAluno(Aluno aluno);
-void alterarNotaDoAluno(Aluno aluno);
-void apagarArquivo();
-int countInd(Aluno aluno);
-void validaNome(char nome[]);
-float validarNotas(int i);
-void deletarAluno(Aluno aluno);
+    void acrescentarDados(Aluno aluno);
+    void listarDados(Aluno aluno);
+    void consultarAluno(Aluno aluno);
+    void alterarNotaDoAluno(Aluno aluno);
+    void apagarArquivo();
+    int countInd(Aluno aluno);
+    void validaNome(char nome[]);
+    float validarNotas(int i);
+    void deletarAluno(Aluno aluno);
+
+#endif
--- Headers/menu.h
+++ Headers/menu.h
@@ -1,12 +1,13 @@
 #ifndef __MENU_H__
 #define __MENU_H__
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <ctype.h>
+    #include <regex.h>
+
+    void cabecalho(char *titulo);
+    int menu();
+    
 #endif
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <regex.h>
-
-void cabecalho(char *titulo);
-int menu();
